ecf5eb8d8d9e53be09164d0cede8e74ed5020fa6
mahesh.ks4 <mahesh.ks4@gmail.com>
2019-09-09 20:20:55 +0530

    rabbitmq EXERCICES

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace NewTask
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+
+                for (int i = 0; i < 10; i++)
+                {
+                    string message = "Hello World! " + i;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                  routingKey: "task_queue",
+                                  basicProperties: properties,
+                                  body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    Thread.Sleep(1000);
+                }
+
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
+
+
